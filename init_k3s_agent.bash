58d916ea47efab0d09b14da5a3b01fa77ef5dc9d
Georgios Kyriopoulos <61064465+gkyriopoulos@users.noreply.github.com>
2025-05-11 18:46:39 +0300

    test2

--- init_k3s_agent.bash
+++ init_k3s_agent.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export MASTER_IP="10.0.0.5" 
+export MASTER_IP="20.61.112.89" 
 export REMOTE_USER="azureuser"
 
 export TOKEN="K105cf45522e56a0a38cda2a96d4a1fe39d81f7f62df280100dc2e827c9cade7d8b::server:651136fe1619598a4330a30bfa32caa0"
