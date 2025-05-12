fc0b9eb6af3418d0c22cec322a379c1c99d7c1ed
Georgios Kyriopoulos <61064465+gkyriopoulos@users.noreply.github.com>
2025-05-12 14:44:55 +0300

    Fixed bugs and added uninstall scripts

--- init_k3s_agent.bash
+++ init_k3s_agent.bash
@@ -6,9 +6,9 @@ export MASTER_USER="azureuser"
 mkdir -p ~/master_info
 
 # Copying the master token file from the master node
-scp -i ~/.ssh/Test1_key.pem $MASTER_USER@$MASTER_IP:~/master_info.yaml ~/master_info/
+scp -i ~/.ssh/Test1_key.pem $MASTER_USER@$MASTER_IP:~/master_info/master_token.yaml ~/master_info/
 
 # Reading the yaml file to get the k3s token
-export K3S_TOKEN =$(grep -oP '(?<=token: ).*' ~/master_info/master_token.yaml)
+export K3S_TOKEN=$(grep -oP '(?<=token: ).*' ~/master_info/master_token.yaml)
 
-curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$K3S_TOKEN sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$K3S_TOKEN sh -s
--- init_k3s_master.bash
+++ init_k3s_master.bash
@@ -4,12 +4,10 @@
 curl -sfL https://get.k3s.io | sh -
 
 # Wait for the K3S service to start
-while ! systemctl is-active --quiet k3s; do
-    sleep 1
-done
+sleep 30
 
 mkdir -p ~/master_info
-K3S_TOKEN=$(cat /var/lib/rancher/k3s/server/node-token)
+K3S_TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
 
 #Saving the token in a yaml file
 echo "token: $K3S_TOKEN" > ~/master_info/master_token.yaml
